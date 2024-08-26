29e8628f06f0e7d9eeaece25cd4bbe4f594085dd
AladdinJr12 <113104184+AladdinJr12@users.noreply.github.com>
2024-08-26 17:13:00 +0800

    Add files via upload
    Adding in files used for the react components and the css files affecting them

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+//import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Question_settings from './components/Questions-settings';
+import QuestionsPage from './components/QuestionsPage';
+import Profile from './components/Profile';
+import Topics from './components/Topics';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path ="/question-settings" element = {<Question_settings />} />
+        <Route path ="/questions-page" element = {<QuestionsPage />} />
+        <Route path ="/profile" element = {<Profile />} />
+        <Route path ="/topics" element = {<Topics />} />
+      </Routes>
+
+
+    </div>
+  );
+}
+
+export default App;
