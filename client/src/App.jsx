f0b785ea2fff9c4862a88447d2f02a829a68b0a8
AladdinJr12 <113104184+AladdinJr12@users.noreply.github.com>
2024-09-05 06:17:00 +0800

    Removing unnecessary lines + route to lessonPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,25 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './css files/App.css'
 
-//import React from 'react';
+import './css files/App.css'
 import { Routes, Route } from 'react-router-dom';
 import Question_settings from './components/Questions-settings';
 import QuestionsPage from './components/QuestionsPage';
 import Profile from './components/Profile';
 import Topics from './components/Topics';
 import HomePage from './components/HomePage';
-import React from 'react';
-
+import LessonPage from './components/LessonPage';
 
 function App() {
   return (
     <div>
-     
       <Routes>
         <Route path ="/" element = {<HomePage />} />
         <Route path ="/question-settings" element = {<Question_settings />} />
         <Route path ="/questions-page" element = {<QuestionsPage />} />
         <Route path ="/profile" element = {<Profile />} />
         <Route path ="/topics" element = {<Topics />} />
+        <Route path ="/lesson-page" element = {<LessonPage />} />
       </Routes>
-
-
     </div>
   );
 }
-
 export default App;
